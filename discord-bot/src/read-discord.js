aee6286c8653765ee72f434a797acdb581ed693d
Alparkom <alparkom@gmail.com>
2025-09-15 20:27:08 -0600

    Se eliminan tokens..

--- discord-bot/src/read-discord.js
+++ discord-bot/src/read-discord.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws')
 const zlib = require('zlib')
+const config = require("./../config.json")
 
 const ws = new WebSocket('wss://gateway.discord.gg/?v=9&encoding=json&compress=zlib-stream')
 
@@ -35,39 +36,7 @@ ws.on('message', (data) => {
 ws.on('open', () => {
   console.log('Conectado al gateway de Discord')
 
-  const identifyPayload = {
-    op: 2,
-    d: {
-      token: 'MTQxMTU0ODQ3MTE2ODczMzIxNQ.GspDYw.bj07JVUKhGnjZVQSTooFeb0vtWn_HB49i1MNvA',
-      capabilities: 1734653,
-      properties: {
-        os: 'Windows',
-        browser: 'Chrome',
-        device: '',
-        system_locale: 'en-US',
-        browser_user_agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
-        browser_version: '139.0.0.0',
-        os_version: '10',
-        referrer: '',
-        referring_domain: '',
-        referrer_current: '',
-        referring_domain_current: '',
-        release_channel: 'stable',
-        client_build_number: 440373,
-        client_event_source: null,
-        has_client_mods: false,
-        client_launch_id: 'b2a66535-da31-4839-9806-65f6fe08308b',
-        launch_signature: '370164ef-27eb-4053-9b78-08b2f038c191',
-        client_heartbeat_session_id: 'f72eac14-633b-41fc-9b91-92d26092b9d8',
-        client_app_state: 'focused',
-        is_fast_connect: false,
-        gateway_connect_reasons: 'AppSkeleton',
-      },
-      presence: { status: 'unknown', since: 0, activities: [], afk: false },
-      compress: true,
-      client_state: { guild_versions: {} },
-    },
-  }
+  const identifyPayload = config.userPayload;
 
   ws.send(JSON.stringify(identifyPayload))
 
