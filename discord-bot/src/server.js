d5e7af93927159d393d50d4952bfbb69a603cbd8
Alparkom <alparkom@gmail.com>
2025-09-15 22:30:57 -0600

    Mejoras para ocupar ssl.

--- discord-bot/src/server.js
+++ discord-bot/src/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const http = require("http");
+const https = require("https");
 const { Server } = require("socket.io");
+const fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("./../cert/key.pem"),
+  cert: fs.readFileSync("./../cert/cert.pem")
+};
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const server = https.createServer(options, app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
 
 io.on("connection", (socket) => {
   console.log("Cliente conectado:", socket.id);
@@ -14,8 +23,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(8080, () => {
-  console.log("Servidor escuchando en :8080");
+server.listen(443, () => {
+  console.log("Servidor escuchando en :443");
 });
 
 module.exports = {
