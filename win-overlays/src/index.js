a1b6eee447758a39321389abd2c74b885bd5f872
Alparkom <alparkom@gmail.com>
2025-10-04 12:26:05 -0300

    Mejora..

--- win-overlays/src/index.js
+++ win-overlays/src/index.js
@@ -1,13 +1,13 @@
-const { app, BrowserWindow, screen, ipcMain, Tray, Menu } = require("electron");
-const path = require("path")
-const fs = require("fs")
+const { app, BrowserWindow, screen, ipcMain, Tray, Menu } = require('electron')
+const path = require('path')
+const fs = require('fs')
 
-let win;
+let win
 
 app.whenReady().then(() => {
-  const { width } = screen.getPrimaryDisplay().workAreaSize;
-  const winWidth = 480;
-  const winHeight = 678;
+  const { width } = screen.getPrimaryDisplay().workAreaSize
+  const winWidth = 480
+  const winHeight = 678
 
   win = new BrowserWindow({
     width: winWidth,
@@ -22,47 +22,53 @@ app.whenReady().then(() => {
     hasShadow: false,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
+      contextIsolation: false,
+    },
+  })
 
-  win.loadFile("./html/index.html");
+  if (process.platform === 'darwin') {
+    win.setVisibleOnAllWorkspaces(true, {
+      visibleOnFullScreen: true,
+    })
+  }
 
-  win.setIgnoreMouseEvents(true, { forward: true });
+  win.loadFile('./html/index.html')
 
-  const iconPath = path.join(__dirname, "..", "taskbar.png");
-  const tray = new Tray(iconPath);
+  win.setIgnoreMouseEvents(true, { forward: true })
+
+  const iconPath = path.join(__dirname, '..', 'taskbar.png')
+  const tray = new Tray(iconPath)
 
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: "Salir",
+      label: 'Salir',
       click: () => {
-        app.quit();
+        app.quit()
       },
     },
-  ]);
+  ])
 
-  tray.setToolTip("Killfeed Overlay");
-  tray.setContextMenu(contextMenu);
+  tray.setToolTip('Killfeed Overlay')
+  tray.setContextMenu(contextMenu)
 
-  ipcMain.on("log", (event, msg) => {
-    console.log("Log desde renderer:", msg);
-  });
+  ipcMain.on('log', (event, msg) => {
+    console.log('Log desde renderer:', msg)
+  })
 
-  win.webContents.once("did-finish-load", () => {
-    const weaponsDir = path.join(__dirname, "..", "assets", "weapons");
+  win.webContents.once('did-finish-load', () => {
+    const weaponsDir = path.join(__dirname, '..', 'assets', 'weapons')
 
-    const files = fs.readdirSync(weaponsDir);
+    const files = fs.readdirSync(weaponsDir)
 
-    const weaponImages = {};
-    files.forEach(file => {
-      const ext = path.extname(file).toLowerCase();
-      if ([".webp", ".png", ".jpg", ".jpeg"].includes(ext)) {
-        const name = path.parse(file).name;
-        weaponImages[name] = path.join(__dirname, "..", "assets", "weapons", file);
+    const weaponImages = {}
+    files.forEach((file) => {
+      const ext = path.extname(file).toLowerCase()
+      if (['.webp', '.png', '.jpg', '.jpeg'].includes(ext)) {
+        const name = path.parse(file).name
+        weaponImages[name] = path.join(__dirname, '..', 'assets', 'weapons', file)
       }
-    });
+    })
 
-    win.webContents.send("weapons-data", weaponImages);
-  });
-});
+    win.webContents.send('weapons-data', weaponImages)
+  })
+})
